f82bc9cf370c6c011bea684631fe179203358a15
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-01-19 19:08:26 +0100

    live tuning long PI

--- common/op_params.py
+++ common/op_params.py
@@ -27,7 +27,8 @@ def read_params(params_file, default_params):
 class opParams:
   def __init__(self):
     self.default_params = {'camera_offset': {'default': 0.06, 'allowed_types': [float, int], 'description': 'Your camera offset to use in lane_planner.py', 'live': True},  # float and int is important
-                           'LONG_P': {'default':  0.5, 'allowed_types': [float, int], 'description': 'Long tuning P', 'live': True}}
+                           'long_P': {'default':  0.5, 'allowed_types': [float, int], 'description': 'Long tuning P', 'live': True},
+                           'long_I': {'default':  0.12, 'allowed_types': [float, int], 'description': 'Long tuning I', 'live': True}}
 
     self.params = {}
     self.params_file = "/data/op_params.json"
