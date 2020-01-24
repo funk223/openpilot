cf28dbf505fa9091dccee8c7826ffee667fd57eb
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-01-24 22:21:32 +0100

    live tuning

--- common/op_params.py
+++ common/op_params.py
@@ -26,10 +26,12 @@ def read_params(params_file, default_params):
 
 class opParams:
   def __init__(self):
-    self.default_params = {'camera_offset': {'default': 0.06, 'allowed_types': [float, int], 'description': 'Your camera offset to use in lane_planner.py', 'live': True},  # float and int is important
-                           'long_P': {'default': 0.5, 'allowed_types': [float, int], 'description': '', 'live': True},
-                           'long_I': {'default': 0.12, 'allowed_types': [float, int], 'description': '', 'live': True},
-                           'steer_ratio': {'default': 13., 'allowed_types': [float, int], 'description': '', 'live': True}}
+    self.default_params = {'camera_offset': {'default': 0.00, 'allowed_types': [float, int], 'description': 'EON camera out of center in m', 'live': True},  # float and int is important
+                           'long_P': {'default': 0.5, 'allowed_types': [float, int], 'longitudinalTuning P': '', 'live': True},
+                           'long_I': {'default': 0.12, 'allowed_types': [float, int], 'longitudinalTuning I': '', 'live': True},
+                           'steer_ratio': {'default': 13., 'allowed_types': [float, int], 'description': '', 'live': True},
+                           'lat_P': {'default': 0.3, 'allowed_types': [float, int], 'lateralTuning P': '', 'live': True},
+                           'lat_I': {'default': 0.05, 'allowed_types': [float, int], 'lateralTuning I': '', 'live': True}}
 
     self.params = {}
     self.params_file = "/data/op_params.json"
