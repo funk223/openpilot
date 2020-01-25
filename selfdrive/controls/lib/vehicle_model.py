a1aba8f20216b5c204e719fb69288d33ac83415e
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-01-25 10:40:55 +0100

    Update vehicle_model.py

--- selfdrive/controls/lib/vehicle_model.py
+++ selfdrive/controls/lib/vehicle_model.py
@@ -109,6 +109,7 @@ class VehicleModel():
     self.cF_orig = CP.tireStiffnessFront
     self.cR_orig = CP.tireStiffnessRear
     self.op_params = opParams()
+
     self.update_params(1.0, CP.steerRatio)
 
   def update_params(self, stiffness_factor, steer_ratio):
